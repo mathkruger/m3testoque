e2daa608b07eb5210c42af59b38ba1c27f2e5be0
Matheus S. Kruger <ms-kruger@hotmail.com>
2017-02-02 18:33:22 -0300

    Update set_odbc.bat

--- M3T Estoque/set_odbc.bat
+++ M3T Estoque/set_odbc.bat
@@ -1,9 +1,7 @@
-set ODBC-params=CONFIGSYSDSN "Microsoft Access Driver (*.mdb, *.accdb)" "DSN=Estoque;DBQ=C:\M3TEstoque\db\Estoque.accdb
-
-%WINDIR%\System32\odbcconf.exe %ODBC-params% 
-if %PROCESSOR_ARCHITECTURE%==AMD64 %WINDIR%\SysWOW64\odbcconf.exe %ODBC-params%
-
+set ODBC-params=CONFIGSYSDSN "Microsoft Access Driver (*.mdb, *.accdb)" "DSN=Estoque;DBQ=C:\M3T Estoque\db\Estoque.accdb
 cls
+if %PROCESSOR_ARCHITECTURE%==AMD64 %WINDIR%\SysWOW64\odbcconf.exe %ODBC-params% else %WINDIR%\System32\odbcconf.exe %ODBC-params% 
+cls
+@echo off
 @echo Fonte de dados configurada com sucesso!
-
 pause
